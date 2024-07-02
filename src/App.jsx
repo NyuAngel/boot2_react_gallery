15b497e12b2065dad098c3ee0ddc0e12021c3916
Viviana Angel <lvfandino@misena.edu.co>
2024-07-02 17:01:40 -0500

    Routes configuration with react-router-dom for SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import './App.css'
-import { Draw1 } from './components/Draw1'
-import { Draw2 } from './components/Draw2'
-import { Draw3 } from './components/Draw3'
-import { Draw4 } from './components/Draw4'
-import { Draw5 } from './components/Draw5'
-import { Draw6 } from './components/Draw6'
+import './App.css';
+import { BrowserRouter, Routes ,Route } from 'react-router-dom';
+import { Draw1 } from './components/Draw1';
+import { Draw2 } from './components/Draw2';
+import { Draw3 } from './components/Draw3';
+import { Draw4 } from './components/Draw4';
+import { Draw5 } from './components/Draw5';
+import { Draw6 } from './components/Draw6';
 
 
 function App() {
   
 
   return (
-    <>
-     <Draw1 />
-     <Draw2 />
-     <Draw3 />
-     <Draw4 />
-     <Draw5 />
-     <Draw6 />
-
+    <>  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/draw1" element = {<Draw1 />} />
+        <Route path="/draw2" element = {<Draw2 />} />
+        <Route path="/draw3" element = {<Draw3 />} />
+        <Route path="/draw4" element = {<Draw4 />} />
+        <Route path="/draw5" element = {<Draw5 />} />
+        <Route path="/draw6" element = {<Draw6 />} />
+      </Routes>
+     </BrowserRouter>
     </>
   )
 }
